2c73aed3c76c8f5d8f52d265e31f0d94f68938cc
Jhon98E <jhonenriquez1998@gmail.com>
2024-10-29 17:43:22 -0500

    Nuevos cambios

--- consumo_universitario/views.py
+++ consumo_universitario/views.py
@@ -1,10 +1,76 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+preguntas = {
+    "p1": {
+        "titulo": "Uso de dispositivos electrónicos",
+        "pregunta": "¿Cuántas horas al día usas dispositivos electrónicos (laptop, celular, tablet) para estudiar o trabajar?",
+        "unidad": ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"]
+    },
+    "p2": {
+        "titulo": "Energía en la universidad",
+        "pregunta": "¿Con qué frecuencia utilizas los enchufes para cargar tus dispositivos electrónicos dentro del campus de la universidad?",
+        "unidad": ["nunca","ocasionalmente","frecuentemente","siempre"]
+    },
+    "p3": {
+        "titulo": "Desperdicio Energetico",
+        "pregunta": "¿Con qué frecuencia dejas tus dispositivos conectados después de que se han cargado por completo?",
+        "unidad": ["nunca","ocasionalmente","frecuentemente","siempre"]
+    },
+    "p4": {
+        "titulo": "Energia en las aulas",
+        "pregunta": "¿Al finalizar las clases desconectan los dispositivos y apagan las luces del aula?",
+        "unidad": ["nunca","ocasionalmente","frecuentemente","siempre"]
+    },
+    "p5": {
+        "titulo": "Uso de baños",
+        "pregunta": "¿Con qué frecuencia utiliza los baños de la universidad durante la semana?",
+        "unidad": ["nunca","1 a 5","6 a 10","11 a 15","16 a 20","21 a 25"]
+    },
+    "p6": {
+        "titulo": "Cerrar la llave",
+        "pregunta": "¿Cierra la llave mientras se cepilla los dientes o se lava las manos?",
+        "unidad": ["nunca","ocasionalmente","frecuentemente","siempre"]
+    },
+    "p7": {
+        "titulo": "Hidratacion",
+        "pregunta": "¿Con que frecuencia utilizas las fuentes o bebederos de agua de la universidad al Dia?",
+        "unidad": ["1","2","3","4","5","6","7","8","9","10"]
+    },
+    "p8": {
+        "titulo": "Desperdicios",
+        "pregunta": "¿Qué tipo de residuos arrojas al sanitario de la universidad?",
+        "unidad": ["ninguno","loquidos","solidos","ambos"]
+    },
+    "p9": {
+        "titulo": "Manejo de residuos",
+        "pregunta": "¿Antes de desechar tu basura conoces en que recipiente debe de ir?",
+        "unidad": ["No","Si"]
+    },
+    "p10": {
+        "titulo": "Reutilizacion de recipientes",
+        "pregunta": "¿Sueles reutilizar las botellas plásticas o prefieres comprar una nueva cada vez que necesitas agua?",
+        "unidad": ["nunca","ocasionalmente","frecuentemente","siempre"]
+    },
+    "p11": {
+        "titulo": "Compartir transporte",
+        "pregunta": "¿Sueles compartir tus medios de transporte con otros compañeros para reducir la huella de carbono?",
+        "unidad": ["Nunca","Rara vez","Ocasionalmente","Frecuentemente","Siempre"]
+    },
+    "p12": {
+        "titulo": "Medio de transporte",
+        "pregunta": "¿Sueles caminar o usar bicicleta para llegar a la universidad? ¿Con qué frecuencia?",
+        "unidad": ["Nunca","Ocasionalmente","Frecuentemente","siempre"]
+    }
+}
 
 
 def home(request):
     return render(request, 'home.html')
 
-
 def calcular_huella(request):
-    return render(request, 'calcular_huella.html')
+    context = {
+        'preguntas': preguntas
+    }
+    
+    return render(request, 'calcular_huella.html', context)
